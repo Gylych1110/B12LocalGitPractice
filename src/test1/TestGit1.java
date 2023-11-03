798257c8cecd4dee32a7b070feba644e4c78a865
Gylych G <gylych1110@gmail.com>
2023-11-03 23:45:54 +0100

    TestGit1 Update

---
+++ src/test1/TestGit1.java
@@ -0,0 +1,12 @@
+package Test1;
+
+public class TestGit1 {
+    public static void main(String[] args) {
+        System.out.println("GIT");
+        System.out.println("Test");
+        System.out.println("Test 2 ");
+        System.out.println("Test 1");
+        System.out.println("Test 4");
+        System.out.println("Test 5");
+    }
+}

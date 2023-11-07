d158cdc187d09855ff2d74a66afab8ce46812f65
Gylych1110 <149843995+Gylych1110@users.noreply.github.com>
2023-11-07 22:03:11 +0100

    Homework

--- src/Test3/HW.java
+++ src/Test3/HW.java
@@ -3,5 +3,6 @@ package Test3;
 public class HW {
     public static void main(String[] args) {
         System.out.println("Start do homework");
+        System.out.println("Update file");
     }
 }
